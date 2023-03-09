71b806e42c5e5bccb797bf778afb992124ffc513
Vasty Kilolo <1653563@students.wits.ac.za>
2023-03-09 14:30:01 +0200

    Cube class complete

--- cube.py
+++ cube.py
@@ -1,87 +1,133 @@
 import numpy as np
 
 class Cube:
+	# ------------------------------------------------------- #
+	# Magic-Cube Constructor								  #
+	# Optional value for cube for testing					  #
+	# ------------------------------------------------------- #
+	def __init__(self, N, cube = np.array([[]])):
+		n, m = np.shape(cube)
 
-	def __init__(self, n, cube = np.array([])):
-		self.n = n if cube.size == 0 else len(cube[0, :])
-		self.cube = np.zeros((n,n)) if cube.size == 0 else cube
+		if not cube.size or n != m:
+			self.n = N
+			self.cube = np.zeros((N,N))
+			self.__generate()
+		else:
+			self.n = n
+			self.cube = cube
 
+	# ------------------------------------------------------- #
+	# Magic-Cube toString									  #
+	# ------------------------------------------------------- #
 	def __str__(self):
-		return "n = %d \n %s" % (self.n, self.cube)
+		return "cube:\n%s\n\nn = %d" % (self.cube, self.n)
 
-	def solve(self):
+
+	# ------------------------------------------------------- #
+	# Magic-Cube Generator                                    #
+	# ------------------------------------------------------- #
+	def __generate(self):
 		i, j = 0, (self.n-1)//2
 
 		self.cube[i, j] = 1
 
-		for num in range(2, self.n**2):
-			k = (i-1) if i >= 1 else (n-1)
-			l = (j-1) if j >= 1 else (n-1)
+		for num in range(2, (self.n**2)+1):
 
-			if self.cube[k, l] >= 1:
-				i = (i+1)%n
-			else:
-				i, j = k, 1
+			k, l = (i-1)%self.n, (j+1)%self.n
+
+
+			if (self.cube[k, l] >= 1) | (i == 0 & j == self.n-1):
+				k, l = i+1, j
+
+			i, j = k, l
 
 			self.cube[i, j] = num
 
+		# print(self.cube)
 
 
+	# ------------------------------------------------------- #
+	# Magic-Cube Validator                                    #
+	# ------------------------------------------------------- #
 	def validate(self):
 		valid = False
 		total, _total = sum(self.cube[0, :]), 0
 
-		#print("total: %d" % (total))
-
+		# Row Validation
 		for i in range(1, self.n):
-			_list = self.cube[i, :]
-			valid, _total = self.__validate(_list, total)
-
-			#print("row %d: %s, \n total: %d" % (i, _list, _total))
+			valid, _total = self.__check(self.cube[i, :], total)
 
 			if not valid:
-				print("validation failed! \n Test Value: %d \n Actual Value: %d" % (total, _total))
+				print("validation failed!\nTest Value: %d\nActual Value: %d" % (total, _total))
 				return False 
 
+
+		# Column Validation
 		for j in range(0, self.n):
-			_list = self.cube[:, j]
-			valid, _total = self.__validate(_list, total)
-
-			#print("col %d: %s, \n total: %d" % (j, _list, _total))
+			valid, _total = self.__check(self.cube[:, j], total)
 
 			if not valid:
-				print("validation failed! \n Test Value: %d \n Actual Value: %d" % (total, _total))
+				print("validation failed!\nTest Value: %d\nActual Value: %d" % (total, _total))
 				return False 
-				
+
+
+		# Diagonals Extraction		
 		pos_diag, neg_diag = np.diag(self.cube), np.diag(self.cube[::-1])[::-1]
 
 
-		valid, _total = self.__validate(pos_diag, total)
-		#print("positive diagonal: %s, \n total: %s" % (pos_diag, _total))
+		# Positive Diagonal Validation
+		valid, _total = self.__check(pos_diag, total)
+
 		if not valid:
-			print("validation failed! \n Test Value: %d \n Actual Value: %d" % (total, _total))
+			print("validation failed!\nTest Value: %d\nActual Value: %d" % (total, _total))
 			return False 
 
-		valid, _total = self.__validate(neg_diag, total)
-		#print("negative diagonal: %s, \n total: %s" % (neg_diag, _total))
+
+		# Negative Diagonal Validation
+		valid, _total = self.__check(neg_diag, total)
+
 		if not valid:
-			print("validation failed! \n Test Value: %d \n Actual Value: %d" % (total, _total))
+			print("validation failed!\nTest Value: %d\nActual Value: %d" % (total, _total))
 			return False
 
-		print("Valid!")
+		# Validation Passed
+		print("Cube:\n%s\nN = %d\nTotal = %d\nValid!" % (self.cube, self.n, total))
 		return True
 
 
-
-	def __validate(self, _list, total):
+	# ------------------------------------------------------- #
+	# List Validation Method                                  #
+	# ------------------------------------------------------- #
+	def __check(self, _list, total):
 		_total = sum(_list)
 		return total == _total, _total
 
 
-a = np.array([[8, 1, 6], [3, 5, 7], [4, 9, 2]])
-cube = Cube(5)
-print(cube)
-cube.solve()
-print(cube)
+a = np.array([
+	          [8, 1, 6],
+	          [3, 5, 7],
+	          [4, 9, 2]
+	        ])
+
+b = np.array([
+			  [17, 24, 1, 8, 15],
+	          [23, 5, 7, 14, 16],
+	          [4, 6, 13, 20, 22],
+	          [10, 12, 19, 21, 3],
+	          [11, 18, 25, 2, 9]
+	         ])
+
+c = np.array([
+			  [30, 39, 48, 1, 10, 19, 28],
+	          [38, 47, 7, 9, 18, 27, 29],
+	          [46, 6, 8, 17, 26, 35, 37],
+	          [5, 14, 16, 25, 34, 36, 45],
+	          [13, 15, 24, 33, 42, 44, 4],
+	          [21, 23, 32, 41, 43, 3, 12],
+	          [22, 31, 40, 49, 2, 11, 20]
+	         ])
+
+cube = Cube(11)
+cube.validate()
 
 
